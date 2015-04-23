9c2df8286e77e109f0ec47b53c06f432014c40df
manuligit <riina.vaisanen@gmail.com>
2015-04-23 17:16:46 +0300

    Refactoring, added javadoc

--- app/src/main/java/test/com/batteryweartest/MainActivity.java
+++ app/src/main/java/test/com/batteryweartest/MainActivity.java
@@ -14,7 +14,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -22,9 +21,13 @@ import java.util.concurrent.ScheduledFuture;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-
+/**
+ * MainActivity for BatteryWearTest:
+ * The program tracks battery usage of the wear watch (though isn't really optical).
+ * It logs battery consumption results through Logcat.
+ */
 public class MainActivity extends Activity {
-    int batterylevel;
+    int batteryLevel;
     TextView textView;
     TextView textView2;
     Intent intent;
@@ -35,16 +38,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //TODO: saada broadcastreceiver toimimaan myös mainactivityssä;
-        // nyt se ei toimi koska receiver käynnistetään Batteryservicessä vasta nappia painaessa, muuten
-        // receiver ei päivitä akunkulutusta kun ohjelma pyörii taustalla
-        // (eli luultavasti pysäytti kokonaan receiverin koska onDestroyssa kutsutaan unregisterReceiveriä)
-
-        //intent = this.registerReceiver(BatteryMonitor, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        //bmData = new BatteryMonitorData();
-        //bmData.getBatteryDataEveryMinute();
-
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -53,18 +46,14 @@ public class MainActivity extends Activity {
                 textView = (TextView) stub.findViewById(R.id.text);
                 textView2 = (TextView) stub.findViewById(R.id.text2);
                 textView2.setText("App is starting");
-                //textView.setText("Battery charge is: " + batterylevel +"%");
                 textView2.setText("");
                 bn1 = (Button) stub.findViewById(R.id.btnaddnewtext1);
                 bn1.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (bmData != null) {
-                            //bmData.getBatteryDataEveryMinute();
-
                             Intent monitorIntent = new Intent(v.getContext(), BatteryService.class);
-                            //Give monitorintent the intent used for BatteryMonitor so the values update in foreground service
-                            //monitorIntent.putExtras(intent);
+                            /** Give monitorintent the intent used for BatteryMonitor so the values update in foreground service */
                             v.getContext().startService(monitorIntent);
                         }
                     }
@@ -74,28 +63,25 @@ public class MainActivity extends Activity {
             }});
     }
 
-
-    //Unregister the receiver on destroy;
-    /*
-    protected void onDestroy() {
-        super.onDestroy();
-        this.unregisterReceiver(BatteryMonitor);
-    }
-    */
+    /**
+     * Get current battery level
+     * @return Integer of current battery level
+     */
     public int getBatteryLevel() {
-        return this.batterylevel;
+        return this.batteryLevel;
     }
 
+    /** Set current battery level. */
     public void setBatteryLevel(int i) {
-        this.batterylevel = i;
+        this.batteryLevel = i;
     }
 
 
 
     /**
      * BroadcastReceiver which listens to changes in the battery state;
-     * updates the batterylevel on MainActivity whenever battery level changes.
-     * Also updates the textview with battery info on MainActivity
+     * updates the batteryLevel on MainActivity whenever battery level changes.
+     * Also updates the TextView with battery info on MainActivity.
     */
     private static BroadcastReceiver BatteryMonitor;
 
@@ -109,27 +95,20 @@ public class MainActivity extends Activity {
             public void onReceive(Context context, Intent intent) {
 
                 level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-                //Set batterylevel for MainActivity every time it changes:
+                /** Set batteryLevel for MainActivity every time it changes: */
                 setBatteryLevel(level);
                 scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
                 Integer status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-
-                //whenever battery is changed, this clause is called:
+                /** Whenever battery is changed, this clause is called: */
                 if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
-
-                    long time = System.currentTimeMillis() / 1000L;
-
                     boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
                     Integer percent = (level * 100) / scale;
 
                     if (textView != null) {
                         if (isCharging) {
-                            //do nothing if the battery is charging
-                            //or start services again if recovering after shutting things down
                             textView.setText("Battery is charging: " + percent + "%");
                         } else if (!isCharging) {
                             if (percent < 20) {
-                                //if battery is low, shut down things here
                                 textView.setText("Battery is getting low: " + percent + "%");
                             } else {
                                 textView.setText("Battery charge is " + percent + "%");
@@ -144,8 +123,7 @@ public class MainActivity extends Activity {
 }
     /**
      * A class to track battery usage every x seconds;
-     *
-     *
+     * Includes a scheduled event which print current battery state every minute in the Logcat log.
      */
 
     public class BatteryMonitorData {
@@ -156,6 +134,7 @@ public class MainActivity extends Activity {
         final ScheduledExecutorService scheduler =
                 Executors.newScheduledThreadPool(1);
 
+        /** Saves battery timestamps and battery level to two different ArrayLists and prints the current data. */
         public void getBatteryDataEveryMinute() {
             Log.d("BatteryMonitorData", "Starting logging");
             times = new ArrayList<>();
@@ -167,32 +146,15 @@ public class MainActivity extends Activity {
                     long time = System.currentTimeMillis() / 1000L;
                     level = getBatteryLevel();
                     Log.i("BatteryMonitorData", "Time: " + (time - start) + " Level: " + level);
-
-                    /*
-                    try {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                long time = System.currentTimeMillis() / 1000L;
-                                level = getBatteryLevel();
-                                //textView2.append("Battery usage time: " + (time - start) + " Level: " + level + "\n");
-                            }
-                        });
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    */
-
                     times.add(time);
                     levels.add(level);
                 }
             };
 
-            //Starts logging items after 10 seconds, and repeats task every x seconds
+            /** Starts logging items after 10 seconds, and repeats task every x seconds */
             final ScheduledFuture loggerHandle = scheduler.scheduleAtFixedRate(logger, 10, 60, SECONDS);
 
-            //Schedule runs for 60*x seconds (= x minutes)
+            /** Schedule runs for 60*x seconds (= x minutes) */
             scheduler.schedule(new Runnable() {
                 public void run() {
                     loggerHandle.cancel(true);
@@ -205,25 +167,26 @@ public class MainActivity extends Activity {
             }, 60 * 60, SECONDS);
         }
 
+        /** Handling recorded data:
+         * After the recording, this method prints stats for the data which has been collected.
+         * Prints total time and battery level dropped.
+         * **/
         public void printData(ArrayList<Long> times, ArrayList<Integer> levels) {
-            /** Handling recorded data: **/
-
             Log.d("BatteryMonitorData", "Handling data: ");
             if (times != null && levels != null) {
                 if (times.size() > 0 && levels.size() > 0) {
                     Long first = times.get(0);
-                    int lastint = times.size()-1;
-                    Long last = times.get(lastint);
+                    int lastInt = times.size()-1;
+                    Long last = times.get(lastInt);
 
                     int max = levels.get(0);
-                    lastint = levels.size()-1;
-                    int min = levels.get(lastint);
+                    lastInt = levels.size()-1;
+                    int min = levels.get(lastInt);
                     int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
 
                     Log.i("BatteryMonitorData2: ", "Battery usage stats: \n Recorded data for " + (last - first) + " seconds. \n Battery level dropped " + ((max - min) * 100 / scale) + "%");
 
                     if (textView2 != null) {
-                        //Log.d("BatteryMonitorData", "textview not null ");
                         final int help = (max - min) * 100 / scale;
                         final long help2 = last - first;
 
@@ -269,7 +232,7 @@ public class MainActivity extends Activity {
             return null;
         }
 
-        //Unregister the receiver on destroy;
+        /** Unregister the receiver on destroy */
         public void onDestroy() {
             Log.d("BatteryService", "Terminating batteryService");
             super.onDestroy();
